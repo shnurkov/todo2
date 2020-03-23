57084f632ba609cb4e124bfa122d564b70977f03
dradamir-afk <G38q@yandex.ru>
2020-03-24 03:17:52 +0700

    add removeAll button"

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="title"> Что бы сделать сегодня?</h2>
+    <form @submit.prevent="onEnter">
+      <input type="text" v-model = "taskItem" placeholder="Что там по делам?">
+    </form>
+    <ToDo v-for="(task, index) in tasks" :key = "index" :task="task"/>
+    
+    <button class="btnRemoveAll" @click = "RemoveAll(tasks)" >Remove All</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*eslint-disable*/
+import ToDo from './components/ToDo.vue'
 
 export default {
-  name: 'App',
+  el: 'App',
   components: {
-    HelloWorld
+    ToDo
+  },
+  data: function(){
+    return{
+      tasks: [
+        {name: "Поесть", status: "false" },
+        {name: "Поспать", status: "false"}
+      ],
+      taskItem: ""
+    };
+  },
+
+  methods: {
+    onEnter(event){
+      if (this.taskItem){
+        this.tasks.push ({name: this.taskItem, status: "false"});
+      }
+      this.taskItem = "";
+    },
+
+    RemoveAll: function(tasks){
+      this.tasks.splice(0);
+    }
+
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btnRemoveAll{
+  margin-top: 10px;
 }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,40 @@
+<template>
+    <p v-bind:class = "{donetask: isDone}">
+        <input type = "checkbox" v-on:cnange="change">
+        <label> {{task.name}}</label>
+        <button class="btnRemove" @click = "Remove(task)" >Remove</button>
+    </p>
+</template>
+
+<script>
+export default {
+    name: "ToDoItem",
+    props: ["task"],
+    data() {
+        return{
+            isDone: false
+        };
+    },
+
+    methods: {
+        change(event) {
+            if (event.target.checked == true){
+                this.isDone = true;
+                this.task.status = true;
+            } else {
+                this.isDone = false;
+                this.task.status = false;
+            }
+            
+        },
+
+    }
+};
+</script>
+
+<style scoped>
+    .donetask {
+        text-decoration: line-through;
+        color: rgb(0, 0, 0);
+    }
+</style>
